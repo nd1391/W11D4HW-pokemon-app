66e55c1690418c41b1ec2cb2ab2fb3045cf6d6cd
Nick <ndavis1391@gmail.com>
2023-04-28 16:41:02 -0400

    connected to database, json browser showing

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-const pokemons = require('./models/pokemon')
+const pokemon = require('./models/pokemon')
 
 // Index
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
+app.get('/pokemon', (req, res) => {
+    res.send({pokemon})
+})
 
 
 // Show
+
 
 // Listen
 app.listen(PORT, () => {
