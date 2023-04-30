c6f3c59e7cd35b5d0a67b92bae66f846bc5e1d8a
Nick <ndavis1391@gmail.com>
2023-04-30 18:17:17 -0400

    viewing all of my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -7,10 +7,22 @@ const myStyle = {
 
 class Index extends React.Component {
     render() {
+        const {pokemon} = this.props
         return (
             <>
             <div style={myStyle}>
                 <h1>See All The Pokemon!</h1>
+                <ul>
+                    {pokemon.map((pokemon, i) => {
+                        return (
+                            <li key={i}>
+                                <a href={`/pokemon/${i}`}>
+                                {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
             </>
         )
