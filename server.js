f1e32112bddf83fc3fa075c4b161865eb2fd5cf0
Nick <ndavis1391@gmail.com>
2023-04-30 17:58:45 -0400

    styled index route

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const pokemon = require('./models/pokemon')
 
+// Middleware
+// const reactViewsEngine = require("jsx-view-engine").createEngine();
+app.engine('jsx', require('jsx-view-engine').createEngine())
+app.set("view engine", "jsx")
+app.set("views", "./views")
+
+
 // Index
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send({pokemon})
+    res.render("Index")
 })
 
 
